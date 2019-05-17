c8a433a504da4e1ce9efc5b0379563546704e4b8
Tehila Rashkas <tehila.rashkes@cs.colman.ac.il>
2019-05-17 18:08:54 +0300

    Add class:Terminal,Ticket.Plane

---
+++ FlyHigh/Models/Plane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyHigh.Models
+{
+    public class Plane
+    {
+        [DisplayName("מספר מטוס")]
+        public int PlaneId { get; set; }
+
+        [DisplayName("שם מטוס")]
+        public string Name { get; set; }
+
+        [DisplayName("סוג מטוס")]
+        public string Model { get; set; }
+
+        [DisplayName("מספר מושבים")]
+        public int NumberOfSeats { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+
+        public ICollection<Seat> Seatses { get; set; }
+
+        public ICollection<PlaneDepartment> PlaneDepartments { get; set; }
+    }
+}
