495b4c34ede0451ccacffb4f2c5cf0b8d763baec
חני דיאמנט <chanidiamant@gmail.com>
2019-05-17 10:34:26 +0300

    Open models: Custumers Department PlaneDepartment Seat

---
+++ FlyHigh/Models/PlaneDepartment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyHigh.Models
+{
+    public class PlaneDepartment
+    {
+        [Key]
+        public int PlaneId { get; set; }
+        public Plane Plane { get; set; }
+        public int DepartmentId { get; set; }
+        public Department department { get; set; }
+    }
+}
---
+++ FlyHigh/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyHigh.Models
+{
+    public class Seat
+    {
+        [DisplayName("מספר כיסא")]
+        public int SeatId { get; set; }
+        public Plane Plane { get; set; }
+        public Ticket Ticket { get; set; }
+    }
+}
