59d170279ce2b53449bd6ac5eddbfceb1ddb534d
Hadar Bruchim <hadarb97@gmail.com>
2019-05-17 11:27:29 +0300

    add models

---
+++ FlyHigh/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace FlyHigh.Models
+{
+    public class Order
+    {
+        [DisplayName("מספר הזמנה")]
+        public int OrderId { get; set; }
+
+        [DisplayName("תאריך הזמנה")]
+        public DateTime Date { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
